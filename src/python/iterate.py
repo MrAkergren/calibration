874f295ec39e465b0c7486d38e45067ce51a7737
MrAkergren <oskar.akergren@gmail.com>
2015-04-01 15:57:48 +0200

    clean up

--- src/python/iterate.py
+++ src/python/iterate.py
@@ -3,19 +3,10 @@
 
 import time
 import numpy
-from colorama import Fore
 import random
+from colorama import Fore
 
 MAX_VALUE = 9
-    
-# array = ([  [0, 0, 0, 0, 0],
-#             [0, 1, 1, 1, 0],
-#             [0, 1, 5, 1, 0],
-#             [0, 1, 4, 10, 0],
-#             [0, 1, 4, 1, 0]     ])
-
-# cols = len(array[0])
-# rows = len(array)
 
 class main():
 
@@ -29,7 +20,6 @@ class main():
                     continue
                 arr[y][x] = MAX_VALUE - (abs(x - xMax) + abs(y - yMax))
 
-        
         # for y in range(0, ySize):
         #     for x in range(0, xSize):
         #         if x == xMax and y == yMax:
@@ -46,30 +36,23 @@ class main():
         yValue = 0
 
         for row in array: 
-            x += 1
             y = 0 #need to zero the y at the beginning of a new row
             for cell in row:
-                y += 1
                 if(cell > value):
                     value = cell
                     xValue = x
                     yValue = y
-
-
-        print("\nPRIMITIVE: ")
+                y += 1
+            x += 1
         print("HIGHETS VALUES WAS:" + str(value) + " on coordinate: x=" + str(xValue) + " y=" + str(yValue))
-       
 
     def labyrinth(self,array):
-        
-        # cols = 
-        # rows = len(array)
 
         #values for the start position in the array
         a = random.randint(0, len(array[0])-2)
         b = random.randint(0, len(array)-2)
 
-        #coordinates for the highest current value in the array 
+        #coordinates for 'value' in the array 
         x = 0
         y = 0
 
@@ -84,80 +67,67 @@ class main():
                     x = a+i
                     y = b+j
 
-        #print("DEVELOP INFORMATION ---- X=" + str(x)+" Y="+str(y)+"\n VALUE="+ str(value))
-
+        #Check clockwise
         while(1):
             #EAST
             if(value < array[x+1][y]):
-                value = array[x+1][y]
                 x += 1
+                value = array[x][y]
 
             #SOUTH EAST
             elif(value < array[x+1][y+1]):
-                value = array[x+1][y+1]
                 x += 1
                 y += 1
+                value = array[x][y]
 
             #SOUTH
             elif(value < array[x][y+1]):
-                value = array[x][y+1]
                 y += 1
-            
+                value = array[x][y]
+
             #SOUTH WEST
             elif(value < array[x-1][y+1]):
                 x -= 1
                 y -= 1            
+                value = array[x][y]
 
             #WEST
             elif(value < array[x-1][y]):
-                value = array[x-1][y]
                 x -= 1
+                value = array[x][y]
 
             #NORTH WEST
-            elif(value < array[x-1][y-1]):
-                value = array[x-1][y+1]
+            elif(value < array[x-1][y-1]):          
                 x -= 1
                 y += 1
-
+                value = array[x][y]
             #NORTH
             elif(value < array[x][y-1]):
-                value = array[x][y-1]
                 y -= 1
-
+                value = array[x][y]
             #NORTH EAST
             elif(value < array[x+1][y-1]):
-                value =array[x+1][y-1]
                 x += 1
                 y -= 1
+                value = array[x][y]
 
             else:
                 break
 
-
-
-        print("\nLABYRINTH: ")
         print("HIGHETS VALUES WAS:" + str(value) + " on coordinate: x=" + str(x) + " y=" + str(y))
-        
-    def stepRight(self, y):
-        y += 1
-
-        return(y)   
-
-    def stepDown(self, x):
-        x += 1
-
-        return(x)
 
 main = main()
 array1 = main.createArray(1000, 1000, random.randint(0,999), random.randint(0,999))
 array2 = main.createArray(1000, 1000, random.randint(0,999), random.randint(0,999))
 array3 = main.createArray(1000, 1000, random.randint(0,999), random.randint(0,999))
 start_time_p = time.time()
+print("\nPRIMITIVE: ")
 main.primitive(array1) 
 main.primitive(array2)
 main.primitive(array3)
 print("TOTAL TIME: %s seconds\n" % (time.time() - start_time_p))
 start_time_l = time.time()
+print("\nLABYRINTH: ")
 main.labyrinth(array1)
 main.labyrinth(array2)
 main.labyrinth(array3)
