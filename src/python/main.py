3e5a6134da7200fdf34ca8161862999ccff717e9
MrAkergren <oskar.akergren@gmail.com>
2015-04-24 14:55:55 +0200

    some minor code clean up

--- src/python/main.py
+++ src/python/main.py
@@ -18,7 +18,7 @@ class Main():
 
         # Initial coordinates
         x, y = self.com.get_coordinates()
-        
+
         # Debug output
         print("Start pos: " + str(x) + ", " + str(y))
 
@@ -30,23 +30,23 @@ class Main():
         finished = False
 
         # Check clockwise
-        # The array boundary checks are needed as long as an array is used as 
+        # The array boundary checks are needed as long as an array is used as
         # input to the Com class.
         while not finished:
             # EAST
-            if (last_move == None or last_move == 'EAST') and (x+self.x_offset, y) not in visited:
+            if (last_move is None or last_move == 'EAST') and (x+self.x_offset, y) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self.com.move((x,y), 'EAST')
+                value_read = self.com.move((x, y), 'EAST')
                 visited.add((x+self.x_offset, y))
-                if value < value_read :
+                if value < value_read:
                     x += self.x_offset
                     value = value_read
                     last_move = 'EAST'
 
             # SOUTH
-            elif (last_move == None or last_move == 'SOUTH') and (x, y-self.y_offset) not in visited:
+            elif (last_move is None or last_move == 'SOUTH') and (x, y-self.y_offset) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self.com.move((x,y), 'SOUTH')
+                value_read = self.com.move((x, y), 'SOUTH')
                 visited.add((x, y-self.y_offset))
                 if value < value_read:
                     y -= self.y_offset
@@ -54,9 +54,9 @@ class Main():
                     last_move = 'SOUTH'
 
             # WEST
-            elif (last_move == None or last_move == 'WEST') and (x-self.x_offset, y) not in visited:
+            elif (last_move is None or last_move == 'WEST') and (x-self.x_offset, y) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self.com.move((x,y), 'WEST')
+                value_read = self.com.move((x, y), 'WEST')
                 visited.add((x-self.x_offset, y))
                 if value < value_read:
                     x -= self.x_offset
@@ -64,16 +64,16 @@ class Main():
                     last_move = 'WEST'
 
             # NORTH
-            elif (last_move == None or last_move == 'NORTH') and (x, y+self.y_offset) not in visited:
+            elif (last_move is None or last_move == 'NORTH') and (x, y+self.y_offset) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self.com.move((x,y), 'NORTH')
+                value_read = self.com.move((x, y), 'NORTH')
                 visited.add((x, y+self.y_offset))
                 if value < value_read:
                     y += self.y_offset
                     value = value_read
                     last_move = 'NORTH'
 
-            elif last_move != None:
+            elif last_move is not None:
                 last_move = None
 
             else:
