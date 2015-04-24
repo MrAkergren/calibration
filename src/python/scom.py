0ad4d69cfb291655783040633e81e0e53453f7e9
MrAkergren <oskar.akergren@gmail.com>
2015-04-24 14:56:16 +0200

    some comments and clean up

--- src/python/scom.py
+++ src/python/scom.py
@@ -3,25 +3,27 @@ import re
 from mock_read import MockRead
 from time import sleep
 
+
 class SCom(object):
+    """The class that handles the serial communication to the SP3 panel.
+    """
     def __init__(self, x, y):
         super(SCom, self).__init__()
         self.connection = None
-        self.regex = re.compile('[-+]?[0-9]*\.?[0-9]+')
-        self.x_offset = x
-        self.y_offset = y
-        self.mock = MockRead()
-        self.connect_remote()
+        self.regex = re.compile('[-+]?[0-9]*\.?[0-9]+')  # regex to extract float
+        self.x_offset = x       # determines the size of adj. steps in the sensor
+        self.y_offset = y       # determines the size of adj. steps in the sensor
+        self.mock = MockRead()  # when lightvalue can't be read, use mock
+        self.serial_connect()   # auto connect when object is created
 
     def serial_connect(self):
-        if self.connection is not None:
-            self.connection.close()
-        self.connection = serial.Serial('/dev/tty.SLAB_USBtoUART', 38400, timeout=1)
-        sleep(1)
-
-    def connect_remote(self):
+        """The method to handle the connection to the panel.
+        """
         try:
-            self.serial_connect()
+            if self.connection is not None:
+                self.connection.close()
+            self.connection = serial.Serial('/dev/tty.SLAB_USBtoUART', 38400, timeout=1)
+            sleep(1)
         except serial.SerialException:
             print('Connection failed')
         else:
@@ -39,43 +41,52 @@ class SCom(object):
         pass
 
     def get_log(self):
-        self.serial_write('logga')
+        """Returns a 'logga'-string from the panel as a list of strings.
+        """
+        self._serial_write('logga')
         sleep(2)
         while self.connection.inWaiting() > 0:
-            log = self.connection.readline()
-            log = log.decode(encoding='utf-8')
-        self.serial_write('logga')
+            log = self._serial_read()
+        self._serial_write('logga')
         info = self.regex.findall(log)
-        print(info[4], info[5])
         return info
 
     def get_coordinates(self):
-        for x in range(0, 10):
-            self.serial_write('zeroposx')
-            sleep(1)
+        """ Tries to return the coordinates 10 times, if it failes it returns 
+        None.
 
+        Retrns a tuple of the two floats that is the coordinates.
+        """
+        for x in range(0, 10):
+            self._serial_write('zeroposx')
+            sleep(1)
             while self.connection.inWaiting() > 0:
-                indata = self.connection.readline()
-                indata = indata.decode(encoding='utf-8')
+                indata = self._serial_read()
                 coordinates = self.regex.findall(indata)
                 if(coordinates):
                     return (float(coordinates[0]), float(coordinates[1]))
         return None
 
     def set_x_coordinate(self, num):
-        self.serial_write('zeroposx ' + num)
-        print('position set to: zeroposx ' + num)
+        """'num' needs to be a string.
+        """
+        self._serial_write('zeroposx ' + num)
+        print('position set to: zeroposx ' + num)  # Debug print
         sleep(2)
 
     def set_y_coordinate(self, num):
-        self.serial_write('zeroposy ' + num)
-        print('position set to: zeroposy ' + num)
+        """'num' needs to be a string.
+        """
+        self._serial_write('zeroposy ' + num)
+        print('position set to: zeroposy ' + num)  # Debug print
         sleep(2)
 
     def move(self, coordinates, direction):
+        """The method that sends the move command to the panel.
+        """
         x, y = coordinates
-        check_x = False
-        check_y = False
+        check_x = False  # If the x-value is changed, set the x coordinate
+        check_y = False  # If the y-value is changed, set the y coordinate
 
         if direction == 'EAST':
             x += self.x_offset
@@ -90,20 +101,19 @@ class SCom(object):
             y += self.y_offset
             check_y = True
 
-        if(check_y):
+        if check_y:
             self.set_y_coordinate(str(y))
-        if(check_x):
+        if check_x:
             self.set_x_coordinate(str(x))
 
-        while not self.correct_position():
-            print(self.correct_position())
-            
+        while not self._correct_position():
+            pass
 
         return self.get_value()
 
-    def serial_write(self, command):
+    def _serial_write(self, command):
 
-        command += '\r'
+        command += '\r'  # Append to the command so the panel to executes it
         try:
             if self.connection.write(command.encode('utf-8')) > 0:
                 pass
@@ -116,7 +126,20 @@ class SCom(object):
         except serial.SerialException:
             print('Serial communication failed')
 
-    def correct_position(self):
+    def _serial_read(self):
+        # To decode the information from the panel to a utf-8 string
+
+        text = self.connection.readline()
+        text = text.decode(encoding='utf-8')
+        return text
+
+    def _correct_position(self):
+        # The index 4&5 contains the difference between the set value and
+        # current position of the panel.
+
+        # If the values are below 0.01 it is to be considered that the panel
+        # is in the right place, according to empirical evidence.
+
         log = self.get_log()
         if(abs(float(log[4]) < 0.01) and abs(float(log[5]) < 0.01)):
             return True
@@ -127,5 +150,4 @@ class SCom(object):
 # a.set_x_coordinate('-0.15')
 # b = a.get_log()
 # print(b[4], b[5])
-# print(a.correct_position())
-
+# print(a._correct_position())
