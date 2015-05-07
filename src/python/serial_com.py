e98e5d688ae01de651a6d759a710d438aa2a7b11
Pär Svedberg <rockkuf@gmail.com>
2015-05-07 10:08:27 +0200

    Revert "bug hunt, commented code contains bug somewhere"
    This reverts commit 8e4913eae50dc2a3488225d6730ad4e105de3ae7.

--- src/python/serial_com.py
+++ src/python/serial_com.py
@@ -1,7 +1,7 @@
 import serial
 from time import sleep
-# import sys
-# import glob
+import sys
+import glob
 
 
 class SerialCommunication(object):
@@ -46,31 +46,31 @@ class SerialCommunication(object):
             print('Serial communication failed')
 
     def _serial_read(self):
-        # To decode the information from the panel to a utf-8 string
-        text = self.connection.readline()
-        text = text.decode(encoding='utf-8')
-        return text
+            # To decode the information from the panel to a utf-8 string
+            text = self.connection.readline()
+            text = text.decode(encoding='utf-8')
+            return text
 
-    # def serial_port_list():
-    #     """ Returns a list of available serial ports on a linux or darwin based system
-    #     """
-    #     if sys.platform.startswith('darwin'):
-    #         ports = glob.glob('/dev/tty.*')
-    #         platform = 'darwin'
+    def serial_port_list():
+        """ Returns a list of available serial ports on a linux or darwin based system
+        """
+        if sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+            platform = 'darwin'
         
-    #     elif sys.platform.startswith('linux'):
-    #         ports = glob.glob('/dev/tty[A-Za-z]*')
-    #         platform = 'linux'
+        elif sys.platform.startswith('linux'):
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+            platform = 'linux'
 
-    #     else:
-    #         raise EnvironmentError('System not supported')
+        else:
+            raise EnvironmentError('System not supported')
 
-    #     serial_ports = []
-    #     for port in ports:
-    #         try:
-    #             s = serial.Serial(port)
-    #             s.close()
-    #             serial_ports.append(port)
-    #         except (OSError, serial.SerialException):
-    #             pass
-    #     return platform, serial_ports
+        serial_ports = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                serial_ports.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return platform, serial_ports
