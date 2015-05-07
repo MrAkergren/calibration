4a89e63025bcd2e5bcbb7f0fc78d73de77fd12ae
MrAkergren <oskar.akergren@gmail.com>
2015-05-07 10:04:32 +0200

    moved the try/except catch

--- src/python/search.py
+++ src/python/search.py
@@ -23,7 +23,7 @@ class Search:
         sun_sensor = True
 
         # Debug output
-        print("Start pos: " + str(start_x) + ", " + str(start_y))
+        print("Start pos: %.4f, %.4f" %(start_x, start_y)) #str(start_x) + ", " + str(start_y))
 
         steps = 0       # Checked for debugging
         last_move = None
@@ -33,75 +33,60 @@ class Search:
         # The array boundary checks are needed as long as an array is used as
         # input to the Com class.
         while not finished:
-            # If coordinates has gone astray or search has been going on too long: abort
-            if self._out_of_bounds(x, y) or self._search_timeout(start_time):
-                break
+            try:
+                # If coordinates has gone astray or search has been going on too long: abort
+                if self._out_of_bounds(x, y) or self._search_timeout(start_time):
+                    break
 
-            # for dir in ['EAST', 'SOUTH','WEST','NORTH']:
-            #     _step(self, dir)
-
-            # EAST
-            elif (last_move is None or last_move == 'EAST') and (x+self.x_offset, y) not in visited:
-                steps += 1      # Counted for debugging
-                try:
+                # EAST
+                elif (last_move is None or last_move == 'EAST') and (x+self.x_offset, y) not in visited:
+                    steps += 1      # Counted for debugging
                     value_read = self._read_value((x, y), 'EAST')  #self.com.move((x, y), 'EAST')
-                except:
-                    sun_sensor = False
-                    break
-                visited.add((x+self.x_offset, y))
-                if value < value_read:
-                    x += self.x_offset
-                    value = value_read
-                    last_move = 'EAST'
+                    visited.add((x+self.x_offset, y))
+                    if value < value_read:
+                        x += self.x_offset
+                        value = value_read
+                        last_move = 'EAST'
 
-            # SOUTH
-            elif (last_move is None or last_move == 'SOUTH') and (x, y-self.y_offset) not in visited:
-                steps += 1      # Counted for debugging
-                try:
+                # SOUTH
+                elif (last_move is None or last_move == 'SOUTH') and (x, y-self.y_offset) not in visited:
+                    steps += 1      # Counted for debugging
                     value_read = self.com.move((x, y), 'SOUTH')
-                except:
-                    sun_sensor = False
-                    break
-                visited.add((x+self.x_offset, y))
-                if value < value_read:
-                    y -= self.x_offset
-                    value = value_read
-                    last_move = 'SOUTH'
+                    visited.add((x+self.x_offset, y))
+                    if value < value_read:
+                        y -= self.x_offset
+                        value = value_read
+                        last_move = 'SOUTH'
 
 
-            # WEST
-            elif (last_move is None or last_move == 'WEST') and (x-self.x_offset, y) not in visited:
-                steps += 1      # Counted for debugging
-                try:
+                # WEST
+                elif (last_move is None or last_move == 'WEST') and (x-self.x_offset, y) not in visited:
+                    steps += 1      # Counted for debugging
                     value_read = self.com.move((x, y), 'WEST')
-                except:
-                    sun_sensor = False
-                    break
-                visited.add((x-self.x_offset, y))
-                if value < value_read:
-                    x -= self.x_offset
-                    value = value_read
-                    last_move = 'WEST'
+                    visited.add((x-self.x_offset, y))
+                    if value < value_read:
+                        x -= self.x_offset
+                        value = value_read
+                        last_move = 'WEST'
 
-            # NORTH
-            elif (last_move is None or last_move == 'NORTH') and (x, y+self.y_offset) not in visited:
-                steps += 1      # Counted for debugging
-                try:
+                # NORTH
+                elif (last_move is None or last_move == 'NORTH') and (x, y+self.y_offset) not in visited:
+                    steps += 1      # Counted for debugging
                     value_read = self.com.move((x, y), 'NORTH')
-                except:
-                    sun_sensor = False
-                    break
-                visited.add((x, y+self.y_offset))
-                if value < value_read:
-                    y += self.y_offset
-                    value = value_read
-                    last_move = 'NORTH'
+                    visited.add((x, y+self.y_offset))
+                    if value < value_read:
+                        y += self.y_offset
+                        value = value_read
+                        last_move = 'NORTH'
 
-            elif last_move is not None:
-                last_move = None
+                elif last_move is not None:
+                    last_move = None
 
-            else:
-                finished = True
+                else:
+                    finished = True
+            except:
+                sun_sensor = False
+                break
 
         # Search did not finish (x or y went out of bounds, or search timeout occured)
         if not finished or not sun_sensor:
@@ -127,4 +112,3 @@ class Search:
 
     def _search_timeout(self, start_time):
         return time()-start_time > self._MAX_TIME
-
