4d96a926fed9b28cbcded032f8a78ac1a0d60d8d
MrAkergren <oskar.akergren@gmail.com>
2015-05-07 09:52:04 +0200

    implemented break on not sun sensor

--- src/python/search.py
+++ src/python/search.py
@@ -38,40 +38,45 @@ class Search:
                 break
 
             # for dir in ['EAST', 'SOUTH','WEST','NORTH']:
-            #     _alg(self, dir)
+            #     _step(self, dir)
 
             # EAST
             elif (last_move is None or last_move == 'EAST') and (x+self.x_offset, y) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self._read_value((x, y), 'EAST')  #self.com.move((x, y), 'EAST')
-                if value_read:
-                    visited.add((x+self.x_offset, y))
-                    if value < value_read:
-                        x += self.x_offset
-                        value = value_read
-                        last_move = 'EAST'
-                else:
+                try:
+                    value_read = self._read_value((x, y), 'EAST')  #self.com.move((x, y), 'EAST')
+                except:
                     sun_sensor = False
                     break
+                visited.add((x+self.x_offset, y))
+                if value < value_read:
+                    x += self.x_offset
+                    value = value_read
+                    last_move = 'EAST'
 
             # SOUTH
             elif (last_move is None or last_move == 'SOUTH') and (x, y-self.y_offset) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self._read_value((x, y), 'SOUTH') #self.com.move((x, y), 'SOUTH')
-                if value_read:
-                    visited.add((x+self.x_offset, y))
-                    if value < value_read:
-                        x += self.x_offset
-                        value = value_read
-                        last_move = 'SOUTH'
-                else:
+                try:
+                    value_read = self.com.move((x, y), 'SOUTH')
+                except:
                     sun_sensor = False
                     break
+                visited.add((x+self.x_offset, y))
+                if value < value_read:
+                    y -= self.x_offset
+                    value = value_read
+                    last_move = 'SOUTH'
+
 
             # WEST
             elif (last_move is None or last_move == 'WEST') and (x-self.x_offset, y) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self.com.move((x, y), 'WEST')
+                try:
+                    value_read = self.com.move((x, y), 'WEST')
+                except:
+                    sun_sensor = False
+                    break
                 visited.add((x-self.x_offset, y))
                 if value < value_read:
                     x -= self.x_offset
@@ -81,16 +86,16 @@ class Search:
             # NORTH
             elif (last_move is None or last_move == 'NORTH') and (x, y+self.y_offset) not in visited:
                 steps += 1      # Counted for debugging
-                value_read = self.com.move((x, y), 'NORTH')
+                try:
+                    value_read = self.com.move((x, y), 'NORTH')
+                except:
+                    sun_sensor = False
+                    break
                 visited.add((x, y+self.y_offset))
                 if value < value_read:
                     y += self.y_offset
                     value = value_read
                     last_move = 'NORTH'
-
-            elif not sun_sensor:
-                print("Sun sensor not active")
-                break
 
             elif last_move is not None:
                 last_move = None
@@ -99,11 +104,13 @@ class Search:
                 finished = True
 
         # Search did not finish (x or y went out of bounds, or search timeout occured)
-        if not finished and sun_sensor:
-            self.com.set_x_coordinate(start_x)
-            self.com.set_y_coordinate(start_y)
+        if not finished or not sun_sensor:
+            self.com.set_x_coordinate(str(start_x))
+            self.com.set_y_coordinate(str(start_y))
             if self.timeout:
                 print("Search timed out (> 5 minutes)")
+            elif not sun_sensor:
+                print("Sun sensor not active, reseting values.")
             else:
                 print("Calibration went out of bounds")
             print("Sensor values are reset")
@@ -120,22 +127,4 @@ class Search:
 
     def _search_timeout(self, start_time):
         return time()-start_time > self._MAX_TIME
-
-    def _read_value(self, coor, dir):
-        try:
-            return self.com.move(coor, dir)
-        except:
-            return False
-
-    # def _alg(self, dir):
-    #        if (last_move is None or last_move == dir) and (x+self.x_offset, y) not in visited:
-    #             value_read = self._read_value((x, y), dir)
-    #             if value_read:
-    #                 visited.add((x+self.x_offset, y))
-    #                 if value < value_read:
-    #                     x += self.x_offset
-    #                     value = value_read
-    #                     last_move = dir
-    #             else:
-    #                 sun_sensor = False
 
